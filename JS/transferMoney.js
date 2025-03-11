49257d9c6d0682b55f3f82e5e0a027314197a202
apurbagoon1 <apurbagoon1@gmail.com>
2025-03-11 19:11:47 +0600

    added transfer money section

---
+++ JS/transferMoney.js
@@ -0,0 +1,16 @@
+document.getElementById("transfer-btn").addEventListener("click", function (event) {
+    event.preventDefault();
+    const pin = document.getElementById("transfer-pin").value;
+    const convertedPin = parseInt(pin);
+    const amount = document.getElementById("transfer-amount").value;
+    convertedAmount = parseFloat(amount);
+    const mainBalance = document.getElementById("main-balance").innerText;
+    const convertedMainBalance = parseFloat(mainBalance);
+    if (convertedPin === 1234) {
+      const sum = convertedMainBalance - convertedAmount;
+      document.getElementById("main-balance").innerText = sum;
+    } 
+    else {
+      alert("Incorrect Pin!");
+    }
+});
